d249b75a5dc5ff99ecaf39e26c16a6e1fc0f3907
gintariukai <gintariukai@hotmail.com>
2023-01-09 15:35:24 +0200

    Add Exceptions4 files

---
+++ src/Exceptions4.java
@@ -0,0 +1,30 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.io.FileNotFoundException;
+
+
+public class Exceptions4 {
+    public static void main(String[] args) {
+        System.out.println("\nHello Exception!\n-------------------");
+
+        try {
+            run();
+        } catch (IOException | ParseException e) { // multi catch
+            e.printStackTrace();
+        }
+
+        /*
+        try {
+            run();
+        } catch (Exception e) {
+            System.out.println("visos išimtis paveldi iš Exception");
+        }
+         */
+
+    }
+
+    public static void run() throws IOException, FileNotFoundException, ParseException, IllegalArgumentException {
+
+    }
+}
